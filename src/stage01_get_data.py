9312a3eec7e4a001c115cfb27710296038ed0782
ChanduKReddy99 <chanduk.amical@gmail.com>
2022-06-17 13:16:55 +0200

    stage01_get_data_completed successfully

---
+++ src/stage01_get_data.py
@@ -0,0 +1,54 @@
+from tqdm import tqdm
+import os
+import argparse
+import logging
+from src.utils.common_utils import read_config_yaml, copy_data,create_dirs
+
+
+logging.basicConfig(
+    filename= os.path.join('logs', 'running_logs.log'),
+    level= logging.INFO,
+    format= '[%(asctime)s: %(levelname)s: %(module)s]:  %(message)s',
+    datefmt= '%Y-%m-%d %H:%M:%S',
+    filemode='a'
+)
+
+def get_data(config_path:str) -> None:
+    """This function gets the data from the source by reading config.yaml file and saves it to the data folder.
+
+    Args:
+        config_path: path to the config.yaml file
+    """
+    config= read_config_yaml(config_path)
+
+    source_data_dirs = config['source_data_download_path']
+    local_data_dirs = config['local_data_path']
+
+    N= len(source_data_dirs)
+    for source_data_dir, local_data_dir in tqdm(zip(source_data_dirs, local_data_dirs),
+            total = N,
+            desc = 'copying directory:',
+            colour= 'green'):
+            
+            create_dirs([local_data_dir])
+            
+            copy_data(source_data_dir, local_data_dir)
+
+
+                    
+            
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Stage 01: Get data')
+    parser.add_argument('--config', '-c', default= 'configs/config.yaml', help='Path to config file')
+    parsed_args= parser.parse_args()
+
+    try:
+        logging.info("\n**************************")
+        logging.info(">>>>>> Stage 01: get_data started.. <<<<<<<<")
+        get_data(parsed_args.config)
+
+    except Exception as e:
+        logging.error(e)
+        logging.error("\nStage 01: get_data failed")
+
+
---
+++ src/utils/common_utils.py
@@ -0,0 +1,54 @@
+from functools import total_ordering
+import os
+import yaml
+import logging
+import shutil
+from tqdm import tqdm
+
+
+
+def read_config_yaml(file_path: str) -> dict:
+    """This function reads the config file and returns the data in a dictionary.
+    
+    args:
+        config_path: path to the config yaml file
+
+    returns:
+        config: dictionary containing the data from the config file
+        """
+    with open(file_path) as yaml_file:
+        content = yaml.safe_load(yaml_file)
+        logging.info('Config file successfully read.')
+        return content
+
+def create_dirs(dir_paths: list) -> None:
+    """This function creates the directories if they do not exist.
+    
+    args:
+        dir_paths: list of paths to the directories to be created
+    """
+    for dir_path in dir_paths:
+            os.makedirs(dir_path, exist_ok=True)
+            logging.info(f'{dir_path} created successfully...')
+
+def copy_data(source_data_dir:str, local_data_dir:str) -> None:
+    """This function copies the data from the source to the destination directory.
+    args:
+        source_data_dir: path to the source data directory
+        local_data_dir: path to the destination data directory
+    """
+    lift_of_files= os.listdir(source_data_dir)
+    N= len(lift_of_files)
+    for file_name in tqdm(lift_of_files, 
+                           total=N, 
+                           desc= f'copying data from {source_data_dir} to {local_data_dir} ...', 
+                           colour='green'):
+                           source= os.path.join(source_data_dir, file_name)
+                           dest= os.path.join(local_data_dir, file_name)
+                           shutil.copy(source, dest)
+    logging.info(f'all the files have been copied from {source_data_dir} to {local_data_dir} successfully...')
+
+
+    
+
+
